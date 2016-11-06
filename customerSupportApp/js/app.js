2da371b88a8e750603432f824a225270769f282b
Hariram Nandagopal <infotech.hari@gmail.com>
2016-11-06 20:50:26 +0530

    Update app.js

--- customerSupportApp/js/app.js
+++ customerSupportApp/js/app.js
@@ -1,7 +1,7 @@
 var currentRequest = "";
 var listenCustpollInterval = "";
 var queuePollerInterval= "";
-var BASE_URL = "http://hariramnandagopal.com/rbsivr_demo/SimplItalk/servicesLayer/";
+var BASE_URL = "http://173.255.234.128:3000/";
 
 function CustomerSupport(){
 

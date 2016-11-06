8ee6c35199995b3e47fc8f87f668ad333d070587
Hariram Nandagopal <infotech.hari@gmail.com>
2016-11-06 20:50:49 +0530

    Update Config.js

--- servicesLayer/Config.js
+++ servicesLayer/Config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	PORT: 3000,
-	BASE_URL: 'http://hariramnandagopal.com/rbsivr_demo/SimplItalk/servicesLayer',
+	BASE_URL: 'http://173.255.234.128:3000/',
 	TWILIO: {
 		ACCOUNT_SID: 'AC0445f1c11149d299a4e647b655bb02f5',
 		AUTH_TOKEN: 'b90e589e41ca618d6d80b963cfde42ed',
